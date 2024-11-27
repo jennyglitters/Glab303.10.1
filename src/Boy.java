16a731afcd06754b9146d3645b17817420241093
Jennipher <voliduser@gmail.com>
2024-11-26 22:17:03 -0500

    Initial commit

--- src/Boy.java
+++ src/Boy.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
-public class Boy {
+public class Boy extends Person {
+    static double    ageFactor = 1.1;
+    public String talk(){
+        return(super.talk() + "... but I love Java class.");
+    }
+    public String walk(){
+        return("I am now walking");
+    }
 }
--- src/Girl.java
+++ src/Girl.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
-public class Girl {
+public class Girl extends Person {
+    static double ageFactor = 1.3;
+    public Girl(String aName){
+        name = "Ms." + aName;
+    }
+    public String talk(){
+        return("Hello!" + jump ());
+    }
+    public String jump(){
+        return("I am jumping.");
+    }
+    public static double lifeSpan(){
+        return(lifeSpan * ageFactor);
+    }
 }
